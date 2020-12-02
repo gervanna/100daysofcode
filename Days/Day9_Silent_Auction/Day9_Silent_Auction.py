75321028aea5a67e19fffd563791a69b6581930b
Gervanna Stephens <stephenslavern@gmail.com>
2020-12-01 20:43:15 -0700

    Update Day9_Silent_Auction.py

--- Days/Day9_Silent_Auction/Day9_Silent_Auction.py
+++ Days/Day9_Silent_Auction/Day9_Silent_Auction.py
@@ -1,4 +1,4 @@
-import os
+import os 
 from auc_art import logo
 
 
@@ -10,15 +10,16 @@ while still_bidding == True:
     print(logo)
     name = input("What is your name?: ")
     bid = int(input("What's your bid?: $"))
-    bidders[name] = bid
+    bidders[name] = bid #adds name and bid to dictionary as key:value
 
-    max_name = max(bidders, key=bidders.get)
-    all_values = bidders.values()
-    max_bid = max(all_values)    
+    max_name = max(bidders, key=bidders.get) #gets the person with the max bid
+    all_values = bidders.values() #stores all the bids in a variable
+    max_bid = max(all_values)    #gets the max bid amounts
     
     next_bidder = input("\nAre there any other bidders? Type 'yes' or 'no': ").lower()
     if next_bidder == 'no':
         still_bidding = False
         print(f"\nThe winner of the Silent Auction is {max_name} with a bid of ${max_bid}.\nCongratulations!")
+        #prints max bidder
     elif next_bidder == 'yes':
-        os.system('clear')
+        os.system('clear') #clears screen in anticipation of next bidder
